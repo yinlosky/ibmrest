07d1a3e5edb27a7a146ac3af3286d1f46524b2f0
yinlosky <yinlosky@yahoo.com>
2014-08-14 14:32:27 -0400

    database connection

--- com.ibm.rest/src/com/ibm/rest/status/V1_status.java
+++ com.ibm.rest/src/com/ibm/rest/status/V1_status.java
@@ -1,6 +1,8 @@
 package com.ibm.rest.status;
 import javax.ws.rs.*;
-import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.*;
+import java.sql.*;
+import com.ibm.dao.*;
 
 /** This is the root path for our restful api service
  *  In the web.xml file, we specified that /api/* need to be in the url to 
@@ -13,10 +15,16 @@ import javax.ws.rs.core.MediaType;
  * @author ibm
  *
  */
-@Path("/v1/status")
+@Path("/v1/status")   // used to route to java class
 public class V1_status {
-	private static final String api_version = "00.01.00"; 
+	private static final String api_version = "00.02.00"; 
 	//only one get method is allowed in one api
+	
+	static public final String driver = "com.microsoft.sqlserver.jdbc.SQLServerDriver";
+	static public final String database= "worlddata";
+	static public final String userName ="sa";
+	static public final String password = "123456";
+	static public final String ip="9.24.170.38";
 	
 	/**
 	 * This method sits at the root of the api. It will return the name of this api.
@@ -38,7 +46,7 @@ public class V1_status {
 	 * @return String - version number of the api   
 	 * 
 	 */
-	@Path("/version")
+	@Path("/version")     // used to route to specific method
 	@GET
 	@Produces(MediaType.TEXT_HTML)
  
@@ -46,5 +54,39 @@ public class V1_status {
 		return "<p>Version:</p>"+api_version;
 	}
 	
-	
+	/** This class uses the mysqldb conn method defined in com.ibm.dao package 
+	 * @return
+	 * @throws Exception
+	 */
+	@Path("/database")
+	@GET
+	@Produces(MediaType.TEXT_HTML)
+	public String returnDatabaseStatus() throws Exception{
+		String connUrl="jdbc:sqlserver://"+ip+";database="+database+";";
+		Connection conn = null;
+		String returnString = null;
+		String myString = null;
+		try{
+			Class.forName(driver);
+			conn = DriverManager.getConnection(connUrl,userName,password);
+			Statement st = conn.createStatement();
+			System.out.println("Try connecting");
+			String sql = "select * from monthly";
+			ResultSet rs = st.executeQuery(sql);
+			while(rs.next()){
+				System.out.println(rs.getString("country"));
+				myString=rs.getString("country");
+			}
+			conn.close();
+			returnString = "<p>Database status</p>"+
+			"<p>Database time/date return: " + myString +"</p>";
+		}
+		catch(Exception e){
+			e.printStackTrace();
+		}
+		finally{
+			if(conn!=null)conn.close();
+		}
+		return returnString;
+	}
 }
